70527c220c31756f8e91f96f2c0613fd12fa5292
William Hawkins <Hawkinswill02@gmail.com>
2026-08-12 09:36:54 +0100

    Add UserAdmin inline for StaffProfile role editing

--- staff/admin.py
+++ staff/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import StaffProfile
 
+class StaffProfileInline(admin.StackedInline):
+    model = StaffProfile
+    can_delete = False
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (StaffProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 @admin.register(StaffProfile)
 class StaffProfileAdmin(admin.ModelAdmin):
